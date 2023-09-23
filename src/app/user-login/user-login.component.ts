74be83eb8646de2234fb32f5ace9bbd780060d5f
kommumahesh9949 <maheshnani9949@gmail.com>
2023-09-23 12:05:48 +0530

    changes for icons

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../services/user.service';
+
+
 
 @Component({
   selector: "app-user-login",
   templateUrl: "./user-login.component.html",
   styleUrls: ["./user-login.component.css"],
 })
+
 export class UserLoginComponent implements OnInit {
+  @ViewChild('successMessage') successMessage: ElementRef;
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
@@ -40,13 +44,25 @@ export class UserLoginComponent implements OnInit {
         console.log(this.userData.userId);
         localStorage.setItem("userId",this.userData.userId);
         this.router.navigate(["/userHome"]);
+        this.showSuccessMessage();
+        setTimeout(() => {
+          this.router.navigate(["/userHome"]);
+        }, 6000); // Delay for 2 seconds (adjust as needed)
       },
+      
       (error) => {
         this.failure.value = true;
         this.loginForm.reset();
       }
     );
   }
+  // Method to display the success message
+showSuccessMessage() {
+  this.successMessage.nativeElement.classList.add('show');
+  setTimeout(() => {
+    this.successMessage.nativeElement.classList.remove('show');
+  }, 100000); // Hide the message after 5 seconds (adjust as needed)
+}
 
 
 }
